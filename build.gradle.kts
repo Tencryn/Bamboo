a364a30de7ffc5457bcdb7e2a923f3075b368c7d
Tencryn <41876297+Tencryn@users.noreply.github.com>
2022-07-24 15:31:35 +0100

    Updated upstream.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG;
-
 plugins {
     java
     `maven-publish`
@@ -7,10 +5,12 @@ plugins {
     id("io.papermc.paperweight.patcher") version "1.3.8"
 }
 
+val paperMavenPublicURL = "https://repo.papermc.io/repository/maven-public/"
+
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    maven(paperMavenPublicURL) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
 
@@ -45,13 +45,7 @@ subprojects {
 
     repositories {
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven(paperMavenPublicURL)
         maven("https://jitpack.io")
     }
 }
